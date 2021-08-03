65c15b753fdecef5c403e68106d45dc12c34f6fd
Dastan <68908934+Dositan@users.noreply.github.com>
2021-08-03 23:07:11 +0600

    ✅ More suitable names.

--- shortify/__main__.py
+++ shortify/__main__.py
@@ -2,26 +2,27 @@ import argparse
 import importlib
 
 
-def get_arguments() -> argparse.Namespace:
-    """`shortify.get_arguments` function, the place where all set args get passed.
+def parse_arguments() -> argparse.Namespace:
+    """`shortify.parse_arguments` function, the place where all set args get passed.
 
     Later on we use this feature in `main` function, the core of the CLI.
     """
-    choices = ('tinyurl', 'git', 'shrtcode', 'isgd', 'clck',)
+    choices = (
+        'tinyurl',
+        'git',
+        'shrtcode',
+        'isgd',
+        'clck',
+    )
     parser = argparse.ArgumentParser(
         'shortify',
         usage='shortify <service> <url to shorten>',
-        description='Shortify CLI!'
+        description='Shortify CLI!',
     )
     parser.add_argument(
-        'service',
-        choices=choices,
-        help='Provide a service you\'d like to use.'
+        'service', choices=choices, help='Provide a service you\'d like to use.'
     )
-    parser.add_argument(
-        'url',
-        help='Pass an URL to shorten.'
-    )
+    parser.add_argument('url', help='Pass an URL to shorten.')
 
     return parser.parse_args()
 
@@ -33,7 +34,7 @@ def main() -> None:
 
     Then we shorenize the URL according to the parsed arguments.
     """
-    args = get_arguments()
+    args = parse_arguments()
     module = importlib.import_module('shortify.services.' + args.service)
     service = module.Shortener()
     print(service.shorten(args.url))
